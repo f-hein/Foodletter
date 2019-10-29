b0154fda696ebff55ce620d913573624c9360da1
Filip Hein <34664324+f-hein@users.noreply.github.com>
2019-10-29 23:29:56 +0100

    Added: handling Culineo fb fanpage
    Edited:
    Extracted FacebookPage to abstract class (uses ABC)
    Rewrote Cockpeat class to inherit from FacebookPage

--- scraper.py
+++ scraper.py
@@ -3,8 +3,47 @@ from datetime import datetime
 
 import requests
 import re
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 from tools import calc_levenshtein
+from abc import ABC, abstractmethod
+
+
+class FacebookPage(ABC):
+    def __init__(self, fanpage_url):
+        self.fanpage_url = fanpage_url
+        self.weekdays = ['poniedzialek', 'wtorek', 'sroda', 'czwartek', 'piatek']
+        self.day_of_the_week = datetime.today().weekday()
+        self._get_page()
+        self._decode_content()
+        self._create_soup_object()
+        self._get_posts_from_content()
+
+    def _get_page(self):
+        self.page = requests.get(self.fanpage_url)
+
+    def _decode_content(self):
+        self.content = self.page.content.decode('utf-8', errors='ignore')
+
+    def _create_soup_object(self):
+        self.soup = BeautifulSoup(self.content, 'lxml')
+
+    def _get_posts_from_content(self):
+        self.posts = self.soup.find_all('div', class_='_427x')
+
+    @staticmethod
+    def _correct_text(text):
+        text = re.sub(',[ ]*$', '', text)
+        text = re.sub(',[ ]+', ', ', text)
+        text = re.sub('[\W]*(,)[\W]*', ', ', text)
+        text = re.sub(r'[ ]+', ' ', text)
+        return text.replace(' ,', ',') \
+            .replace(' :', ': ') \
+            .replace(' *', ' \n') \
+            .replace('  -', '\n  ')
+
+    @abstractmethod
+    def get_todays_menu(self) -> dict:
+        pass
 
 
 class AstraMenu:
@@ -56,44 +95,15 @@ class AstraMenu:
         return text
 
 
-class CockpeatMenu:
+class CockpeatMenu(FacebookPage):
     def __init__(self):
-        self.menu_url = 'https://www.facebook.com/pg/COCKPEAT/posts'
-        self.weekdays = ['poniedzialek', 'wtorek', 'sroda', 'czwartek', 'piatek']
-        self.day_of_the_week = datetime.today().weekday()
-        self._get_page()
-        self._decode_content()
-        self._create_soup_object()
-        self._get_posts_from_content()
+        super().__init__(fanpage_url='https://www.facebook.com/pg/COCKPEAT/posts')
 
     def get_todays_menu(self) -> dict:
         menu = ''
         if self.day_of_the_week < 5:
             menu = self._search_for_menu_by_day_name(self.weekdays[self.day_of_the_week])
         return {'cockpeat_menu': menu}
-
-    def _get_page(self):
-        self.page = requests.get(self.menu_url)
-
-    def _decode_content(self):
-        self.content = self.page.content.decode('utf-8', errors='ignore')
-
-    def _create_soup_object(self):
-        self.soup = BeautifulSoup(self.content, 'lxml')
-
-    def _get_posts_from_content(self):
-        self.posts = self.soup.find_all('div', class_='_427x')
-
-    @staticmethod
-    def _correct_text(text):
-        text = re.sub(',[ ]*$', '', text)
-        text = re.sub(',[ ]+', ', ', text)
-        text = re.sub('[\W]*(,)[\W]*', ', ', text)
-        text = re.sub(r'[ ]+', ' ', text)
-        return text.replace(' ,', ',')\
-                   .replace(' :', ': ')\
-                   .replace(' *', ' \n')\
-                   .replace('  -', '\n  ')
 
     @staticmethod
     def _get_day_from_post(text):
@@ -118,8 +128,55 @@ class CockpeatMenu:
         return todays_menu
 
 
+class ObiadeoMenu(FacebookPage):
+    def __init__(self):
+        super().__init__(fanpage_url='https://www.facebook.com/pg/obiadeo/posts')
+
+    def get_todays_menu(self) -> dict:
+        menu = ''
+        if self.day_of_the_week < 5:
+            menu = self._search_for_menu_by_day_name(self.weekdays[self.day_of_the_week])
+        return {'obiadeo_menu': menu}
+
+    @staticmethod
+    def _get_days_with_dates_from_post(text):
+        text = re.sub(r'[ ]{2,}', ' ', text)
+        try:
+            days_with_dates = re.findall('[a-zA-ZłśŁŚąę]{5,}[ ]+[0-9(.):]+', text)
+            day_and_date_dict = dict()
+            if len(days_with_dates) > 0:
+                for day_and_date in days_with_dates:
+                    if '...' not in day_and_date:
+                        weekday, weekday_date = re.sub(r'[():]', '', day_and_date).split(" ")
+                        if weekday not in day_and_date_dict:
+                            day_and_date_dict[weekday] = weekday_date
+                return day_and_date_dict
+        except IndexError:
+            return ''
+
+    def _search_for_menu_by_day_name(self, searched_day_name):  # TODO: refactor this method
+        todays_menu = ''
+        for post in self.posts:
+            days_with_dates = self._get_days_with_dates_from_post(post.text.lower())
+            if days_with_dates:
+                for day, day_date in days_with_dates.items():
+                    if calc_levenshtein(day, searched_day_name) <= 2 and day_date.count('.') == 1:
+                        now = datetime.now()
+                        date_format = '%d.%m.%Y'
+                        day_date_str = day_date + f'.{now.year}'
+                        day_date_obj = datetime.strptime(day_date_str, date_format)
+                        if day_date_obj.date() == now.date():  # if date and weekday are in post and is current day
+                            menu_entries = post.find_all('p')
+                            for menu_entry in menu_entries:
+                                if day_date in menu_entry.text:
+                                    temp_menu_list = [x for x in menu_entry.contents if type(x) is NavigableString][1:]
+                                    todays_menu += '\n'.join(temp_menu_list).replace('<br/>', '\n')
+                                    break
+        return todays_menu
+
+
 def get_all_menus() -> dict:
-    list_of_classes = [AstraMenu, CockpeatMenu]
+    list_of_classes = [AstraMenu, CockpeatMenu, ObiadeoMenu]
     all_menus = dict()
     for single_menu in map(lambda cls: cls().get_todays_menu(), list_of_classes):
         all_menus.update(single_menu)
